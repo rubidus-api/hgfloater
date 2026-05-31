1a08e4ea295343ffc474474f78a079905931b6ed
Hermes <hermes@localhost>
2026-05-31 11:31:23 +0900

    bump: update version to v26.05.31

--- hg_common.h
+++ hg_common.h
@@ -71,7 +71,7 @@
 
 /* Build-time versioning */
 #ifndef HG_VERSION_W
-#define HG_VERSION_W L"v26.05.30"
+#define HG_VERSION_W L"v26.05.31"
 #endif
 
 /* Include generated About text from README.md if available */
