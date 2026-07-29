88b4bc51a55542590a6cb7d405b82c6dd988b8e8
rubidus-api <rubidus@gmail.com>
2026-07-29 20:29:28 +0900

    release: cut v26.07.29k

--- src/hg_common.h
+++ src/hg_common.h
@@ -71,7 +71,7 @@
 
 /* Build-time versioning */
 #ifndef HG_VERSION_W
-#define HG_VERSION_W L"v26.07.29j"
+#define HG_VERSION_W L"v26.07.29k"
 #endif
 
 /* Include generated About text from README.md if available */
